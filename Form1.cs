32d54962df5eaceeaa53b718945e1b6edbe3bb77
Grihes <90863774+Grihes@users.noreply.github.com>
2021-10-05 00:21:28 +0500

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing.Drawing2D;
+namespace Lab3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+           double[,] array = Program.AvarageArray(Program.CreateArray3());
+          void fillData()
+        {
+            // Взял готовую часть кода с созданием тепловой карты из интернета, изменив частично под свою задачу
+            int maxRow = array.GetLength(0);
+            int maxCol = array.GetLength(1);
+            double k = 200; // Значение коэффициента подбирается для лучшей картины эквипотенциальных поверхностей, тепловой карты. Менял в диапозоне 100<k<2000
+            dataGridView1.RowHeadersVisible = false;
+            dataGridView1.ColumnHeadersVisible = false;
+            dataGridView1.AllowUserToAddRows = false;
+            dataGridView1.AllowUserToOrderColumns = false;
+            dataGridView1.CellBorderStyle = DataGridViewCellBorderStyle.None;
+            //..
+
+            int rowHeight = dataGridView1.ClientSize.Height / maxRow - 1;
+            int colWidth = dataGridView1.ClientSize.Width / maxCol - 1;
+
+            for (int i = 0; i < maxRow; i++) dataGridView1.Columns.Add(i.ToString(), "");
+            for (int i = 0; i < maxRow; i++) dataGridView1.Columns[i].Width = colWidth;
+            dataGridView1.Rows.Add(maxRow);
+            for (int j = 0; j < maxRow; j++) dataGridView1.Rows[j].Height = rowHeight;
+
+            List<Color> baseColors = new List<Color>();  // create a color list
+            baseColors.Add(Color.RoyalBlue);
+            baseColors.Add(Color.LightSkyBlue);
+            baseColors.Add(Color.LightGreen);
+            baseColors.Add(Color.Yellow);
+            baseColors.Add(Color.Orange);
+            baseColors.Add(Color.Red);
+            List<Color> colors = interpolateColors(baseColors, 5000);
+
+            for (int j = 0; j < maxRow; j++)
+            {
+                for (int i = 0; i < maxRow; i++)
+                {
+                    dataGridView1[j, i].Value = Math.Round(array[j, i], 3).ToString();
+                    dataGridView1[j, i].Style.BackColor =
+                                   colors[Convert.ToInt16(Math.Abs( 10 - array[j, i]) * k)];
+
+                }
+            }
+
+        }
+        List<Color> interpolateColors(List<Color> stopColors, int count)
+        {
+            SortedDictionary<float, Color> gradient = new SortedDictionary<float, Color>();
+            for (int i = 0; i < stopColors.Count; i++)
+                gradient.Add(1f * i / (stopColors.Count - 1), stopColors[i]);
+            List<Color> ColorList = new List<Color>();
+
+            using (Bitmap bmp = new Bitmap(count, 1))
+            using (Graphics G = Graphics.FromImage(bmp))
+            {
+                Rectangle bmpCRect = new Rectangle(Point.Empty, bmp.Size);
+                LinearGradientBrush br = new LinearGradientBrush
+                                        (bmpCRect, Color.Empty, Color.Empty, 0, false);
+                ColorBlend cb = new ColorBlend();
+                cb.Positions = new float[gradient.Count];
+                for (int i = 0; i < gradient.Count; i++)
+                    cb.Positions[i] = gradient.ElementAt(i).Key;
+                cb.Colors = gradient.Values.ToArray();
+                br.InterpolationColors = cb;
+                G.FillRectangle(br, bmpCRect);
+                for (int i = 0; i < count; i++) ColorList.Add(bmp.GetPixel(i, 0));
+                br.Dispose();
+            }
+            return ColorList;
+        }
+
+        private void Form1_Load_1(object sender, EventArgs e)
+        {
+            fillData();
+            textBox1.Text = "Минимальное число итераций для усреднения в 1 задании = " + Program.FindCount(Program.CreateArray1()).ToString();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab3
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+        public static double[,] CreateArray1()
+        {
+            Random rnd = new Random();
+            double[,] array = new double[10, 10];
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    if (i == 0 || j == 0 || i == 9 || j == 9)
+                        array[i, j] = 10;
+                    else
+                        array[i, j] = rnd.Next(-10, 10) * 0.05 + 10;
+                }
+            }
+            return array;
+        }
+        public static double[,] CreateArray2()
+        {
+            Random rnd = new Random();
+            double[,] array = new double[10, 10];
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    if (i == 0 || j == 0 || i == 9 || j == 9)
+                        array[i, j] = 10;
+                    else
+                    array[i, j] = 0;
+                }
+            }
+            array[4, 4] = 4;
+            return array;
+        }
+        public static double[,] CreateArray3()
+        {
+            Random rnd = new Random();
+            double[,] array = new double[10, 10];
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    if (i == 0 || i == 9)
+                        array[i, j] = 10;
+                    else if (j == 0 || j == 9)
+                        array[i, j] = 5;
+                    else array[i, j] = 0;
+                }
+            }
+            array[4, 4] = 4;
+            return array;
+        }
+        public static int FindCount (double[,] array)
+        {
+            double[,] arr1 = new double[10, 10];
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                    arr1[i, j] = array[i, j];
+            }
+            int count = 0;
+            int k = 0;
+            while (k != 64)
+            {
+                k = 0;
+                arr1 = GetAvaragedArray(arr1);
+                for (int i = 1; i < 9; i++)
+                {
+                    for (int j = 1; j < 9; j++)
+                    {
+                        if ((Math.Abs(10 - arr1[i, j])) / 10 <= 0.01)
+                            k += 1;
+                    }
+                }
+                count += 1;
+            }
+            return count;
+        }
+        public static double[,] AvarageArray(double[,] array)
+        {
+            int c = 150; // Количество итераций, меняю прямо в программе для нужного задания
+            double[,] arr2 = new double[10, 10];
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                  arr2[i, j] = array[i, j];
+            }
+            for (int i = 0; i < c; i++)
+                arr2 = GetAvaragedArray(arr2);
+            return arr2;
+        }
+        public static double[,] GetAvaragedArray(double[,] array)
+        {
+            double[,] arr = new double[10, 10];
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    if (i == 0 || j == 0 || i == 9 || j == 9)
+                        arr[i, j] = array[i,j];
+                    else
+                        arr[i, j] = 0.25 * (array[i + 1, j] + array[i - 1, j] + array[i, j + 1] + array[i, j - 1]);
+                }
+            }
+            return arr;
+        }
+    }
+}
